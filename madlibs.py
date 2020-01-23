6e426d66f30c874f8b12e902f2bb14cc300dfd70
Your Name <you@example.com>
2020-01-22 17:01:05 -0800

    completed up to game html

--- madlibs.py
+++ madlibs.py
@@ -42,6 +42,19 @@ def greet_person():
                            compliment=compliment)
 
 
+@app.route('/game')
+def show_madlib_form():
+
+    answer = request.args.get("answer")
+
+    if answer == "yes":
+        return render_template("game.html")
+
+    else:
+        return render_template('goodbye.html')
+
+    return render_template("compliment.html",choice=answer)
+
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
